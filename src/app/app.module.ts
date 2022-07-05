40cab228386e4e2f8afe7e86ef555fa0748c5b23
Carlos Alberto Fiallos Bejarano <carlosfiallos@hotmail.es>
2022-07-04 20:38:52 -0500

    First upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,14 +10,21 @@ import { SharedModule } from './components/shared/shared.module';
 //Componentes
 import { LoginComponent } from './components/login/login.component';
 import { DatosComponent } from './components/client/datos/datos.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, DatosComponent],
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		DatosComponent,
+		FileUploadComponent
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
-		SharedModule
+		SharedModule,
+		HttpClientModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/components/client/dashboard/profile/profile.component.ts
+++ src/app/components/client/dashboard/profile/profile.component.ts
@@ -4,24 +4,55 @@ import { Cliente } from 'src/app/components/model/cliente.interface';
 import { ClientService } from '@app/components/client/services/client.service';
 import { Client } from '@app/components/client/models/client.interface';
 import { AuthService } from '@app/services/auth/auth.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FileUploadComponent } from '../../../file-upload/file-upload.component';
 @Component({
 	selector: 'app-profile',
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	panelOpenState = false;
 	client: Client;
+	formulario: FormGroup;
+	frmPareja: FormGroup;
+
+	casadoTexto = 'Casado/a';
+	enUnionTexto = 'En unión de hecho';
+
 	constructor(
 		private router: Router,
 		private clientService: ClientService,
-		private authService: AuthService
+		private authService: AuthService,
+		private fb: FormBuilder
 	) {}
 
 	ngOnInit(): void {
+		this.formulario = this.fb.group({
+			foto: [null],
+			cedula_personal: [null],
+			papeleta_votacion_personal: [null]
+		});
+
+		this.frmPareja = this.fb.group({
+			cedula_conyugue: [null],
+			papeleta_conyugue: [null]
+		});
+
 		const idUser = JSON.parse(this.authService.getProfile()).id;
 		this.clientService.getClientById(idUser).subscribe((data) => {
 			this.client = data;
+			this.emparejado(data);
 		});
+	}
+
+	emparejado(dato: Client) {
+		if (
+			dato.estado_civil.name === this.casadoTexto ||
+			dato.estado_civil.name === this.enUnionTexto
+		) {
+			console.log('Acciones extra');
+		}
 	}
 
 	addCliente(form: Cliente) {}
